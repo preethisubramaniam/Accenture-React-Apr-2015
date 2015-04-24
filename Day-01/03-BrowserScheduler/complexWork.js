6465314b0d33557f4df03b02142a6a98b3bc5d10
tkmagesh <tkmagesh77@gmail.com>
2015-04-24 17:08:35 +0530

    Web Workers

---
+++ Day-01/03-BrowserScheduler/complexWork.js
@@ -0,0 +1,23 @@
+function doWork(){
+    for(var i=0; i<10000; i++){
+        for(var j=0; j<10000; j++)
+            for(var k=0; k<100; k++){}
+        if (i % 100 === 0){
+            self.postMessage({
+                type : "progress",
+                percentCompleted : (i / 10000) * 100
+            });
+        }
+    }
+    self.postMessage({
+        type : "completed"
+    })
+}
+self.addEventListener("message", function(evtArg){
+    if (evtArg.data.type === "start"){
+        doWork();
+    } else {
+        console.log("unknown message ", evtArg.data);
+    }
+
+});
